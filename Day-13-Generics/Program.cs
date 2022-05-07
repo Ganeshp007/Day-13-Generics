640ca1162ab380071eb081b4be3ac7d6168feca0
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-08 00:46:52 +0530

    Added: UC1_FindMaxOf3Int

---
+++ Day-13-Generics/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Day_13_Generics;
+
+Console.WriteLine("---- Find max Number ---- ");
+int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+Console.WriteLine("Maximum Number is:- "+output);
+
+
+
+
